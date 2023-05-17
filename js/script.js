821a4bda571e318aba966e3185fba9343f1d29ee
allynaus <ally.naus@gmail.com>
2023-05-16 18:24:36 -0700

    Created initial pokemonList
    Created a complex JavaScript variable (array) to form a list of types of Pokemon that shows their name, height, and type.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//array of pokemon types and names
+let pokemonList = [
+    {name: 'Bulbasaur', height: '0.7 m', type: 'Poison'},
+    {name: 'Squirtle', height: '0.5 m', type: 'Water'},
+    {name: 'Ponyta', height: '1 m', type: 'fire'}
+];
