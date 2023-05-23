4dd2fc655987f56ee117c45aef7d09eb55e1b855
allynaus <ally.naus@gmail.com>
2023-05-22 19:30:40 -0700

    Updated numerical values
    Changed values from '0.7 m' to '0.7' in order to fix any errors

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 //array of pokemon types and names
 let pokemonList = [
-    {name: 'Bulbasaur', height: '0.7 m', type: 'Poison'},
-    {name: 'Squirtle', height: '0.5 m', type: 'Water'},
-    {name: 'Ponyta', height: '1 m', type: 'fire'}
+    {name: 'Bulbasaur', height: '0.7', type: 'Poison'},
+    {name: 'Squirtle', height: '0.5', type: 'Water'},
+    {name: 'Ponyta', height: '1', type: 'fire'}
 ];
 console.log(pokemonList);
 
