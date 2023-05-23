ad64396a044ce3116a8574ed54c535435a1d15a4
allynaus <ally.naus@gmail.com>
2023-05-22 18:35:48 -0700

    Added "for loop"
    Created for loop to display the height of the pokemon in a list with their names

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ let pokemonList = [
     {name: 'Ponyta', height: '1 m', type: 'fire'}
 ];
 console.log(pokemonList);
+
+//adding a loop to create a list of pokemon and their attributes
+for (let i=0; i < pokemonList.length; i++) {
+    document.write 
+    if (pokemonList[i].height <0.10 && pokemonList.height[i].height >0.3){
+        console.log(pokemonList[i].name + " is small");
+    }else if (pokemonList[i].height <0.7){
+        console.log(pokemonList[i].name + " is medium");
+    }else {
+        console.log(pokemonList[i].name + " is big");
+    }
+}
