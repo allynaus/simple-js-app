f4e486342b1c09bce8d4cb8ff1130f1dbccd57fe
allynaus <ally.naus@gmail.com>
2023-05-30 19:29:40 -0700

    Added forEach() loop
    Erased my previous for() loop and replaced it with a forEach() loop in order to be more clear and to call all objects at once.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
 //array of pokemon types and names
 let pokemonList = [
-    {name: 'Bulbasaur', height: '0.7', type: 'Poison'},
-    {name: 'Squirtle', height: '0.5', type: 'Water'},
-    {name: 'Ponyta', height: '1', type: 'fire'}
+    {
+     name: 'Bulbasaur',
+     height: '0.7',
+     type: 'Poison'
+    },
+    {
+     name: 'Squirtle',
+     height: '0.5',
+     type: 'Water'
+    },
+    {
+     name: 'Ponyta',
+     height: '1',
+     type: 'fire'
+    }
 ];
 console.log(pokemonList);
 
-//adding a loop to create a list of pokemon and their attributes
-for (let i=0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height <0.10 && pokemonList.height[i].height >0.3){
-        document.write(<br> + pokemonList[i].name + " is small" + </br>);
-    }else if (pokemonList[i].height <0.7){
-        document.write(<br> + pokemonList[i].name + " is medium" + </br>);
-    }else {
-        document.write(<br> + pokemonList[i].name + " is big" + </br>);
-    }
-}
+//adding a forEach loop to create a list of pokemon and their attributes
+pokemonList.forEach(function(pokemon) {
+    console.log(pokemon.name + ' is ' + pokemon.height + ' this tall. ');
+});
+
